390b28532466f2807fa361413355ae7e5f57ffec
DDillan <76426963+DDillan@users.noreply.github.com>
2022-06-17 19:46:41 +0530

    des commit 3

--- app/src/main/java/com/example/mad_project/MainActivity.java
+++ app/src/main/java/com/example/mad_project/MainActivity.java
@@ -11,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 //        Hello bruv
 //                hows life
+                        life's good bro
+            wby?
     }
 }
