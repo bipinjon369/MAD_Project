a96f6e618815d39849dc890c5395c2024e686713
DDillan <76426963+DDillan@users.noreply.github.com>
2022-06-19 19:30:44 +0530

    des commit 3

--- app/src/main/java/com/example/mad_project/MainActivity.java
+++ app/src/main/java/com/example/mad_project/MainActivity.java
@@ -1,21 +1,39 @@
 package com.example.mad_project;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        Hello bruv
-//                hows life
-          //              life's good bro
-//            wby? fsgdf
-  //                  fghdfgc
-    //                        fcgn
-      //                      sdfgdfh
-        //                            dcfh
+
+        DrawerLayout drawerLayout = findViewById(R.id.drawerlayout);
+
+        findViewById(R.id.imagemenu).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+
+        NavigationView navigationView = findViewById(R.id.navigationview);
+        navigationView.setItemIconTintList(null);
+
+        NavController navController = Navigation.findNavController(this,R.id.navHostFragment);
+        NavigationUI.setupWithNavController(navigationView,navController);
+
+
+
     }
 }
